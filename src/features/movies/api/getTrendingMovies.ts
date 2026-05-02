72def4509b501aa64ffb417c7c3e33f192c9d2b9
Md Faizan Ahmad <mdfaizan7563@gmail.com>
2026-05-02 22:40:52 +0530

    feat: define strict TypeScript types for TMDb movie responses

--- src/features/movies/api/getTrendingMovies.ts
+++ src/features/movies/api/getTrendingMovies.ts
@@ -1,6 +1,10 @@
+import { Movie, PaginatedResponse } from "@/@types/movie.types";
 import { apiClient } from "@/lib/api/client";
 
-export async function getTrendingMovies() {
-  const response = await apiClient.get("/trending/movie/day");
+export async function getTrendingMovies(): Promise<PaginatedResponse<Movie>> {
+  const response = await apiClient.get<PaginatedResponse<Movie>>(
+    "/trending/movie/day",
+  );
+
   return response.data;
 }

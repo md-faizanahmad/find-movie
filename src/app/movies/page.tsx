8d320a055f2ef123115ddb7c6b47db2a50ae49d8
Md Faizan Ahmad <mdfaizan7563@gmail.com>
2026-05-10 08:55:33 +0530

    feat(people): implement dual-dropdown filters and reset logic

--- src/app/movies/page.tsx
+++ src/app/movies/page.tsx
@@ -69,7 +69,7 @@ export default async function MoviesPage({ searchParams }: Props) {
 
             <div className="mx-1 mb-1 hidden h-6 w-px bg-white/10 sm:block" />
 
-            <div className="w-full sm:w-auto mb-">
+            <div className="w-full sm:w-auto mt-1">
               <SortDropdown />
             </div>
           </div>

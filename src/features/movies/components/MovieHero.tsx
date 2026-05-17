04aea617aeef3c19fde7ca4f0824506d74ce39f9
Md Faizan Ahmad <mdfaizan7563@gmail.com>
2026-05-17 08:34:44 +0530

    style: refine movie info typography and improve responsive layout layout

--- src/features/movies/components/MovieHero.tsx
+++ src/features/movies/components/MovieHero.tsx
@@ -39,7 +39,7 @@ export function MovieHero({
   };
 
   return (
-    <section className="relative h-[65vh] md:h-[85vh] w-full overflow-hidden bg-black">
+    <section className="relative mt-8 h-[65vh] md:h-[85vh] w-full overflow-hidden bg-black">
       {/* 1. Base Layer: Visuals */}
       <div className="absolute inset-0 z-0">
         <Image

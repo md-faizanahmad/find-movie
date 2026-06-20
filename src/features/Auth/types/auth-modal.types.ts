791931ebc0817594787ee554519203c098a0acac
Md Faizan Ahmad <mdfaizan7563@gmail.com>
2026-06-20 20:57:31 +0530

    feat(AuthModal)1'

--- src/features/Auth/types/auth-modal.types.ts
+++ src/features/Auth/types/auth-modal.types.ts
@@ -2,20 +2,16 @@ export type AuthStep = "login" | "verify" | "success";
 
 export interface RequestOtpPayload {
   fullName: string;
-
   email: string;
-
   birthYear: number;
 }
 
 export interface VerifyOtpPayload {
   email: string;
-
   otp: string;
 }
 
 export interface AuthResponse {
   success: boolean;
-
   message: string;
 }

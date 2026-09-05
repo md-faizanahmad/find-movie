21107803f1f7400a78ace0b326a7d78cbc2d84b8
Md Faizan Ahmad <mdfaizan7563@gmail.com>
2026-09-05 20:34:42 +0530

    perf: cache Indian TV shows for one hour

--- src/features/movies/api/getIndianTVShows.ts
+++ src/features/movies/api/getIndianTVShows.ts
@@ -1,9 +1,44 @@
+// // src/features/movies/services/getIndianTVShows.ts
+
+// import { PaginatedResponse } from "@/@types/movie.types";
+// import { TMDBTV } from "@/@types/tv.types";
+// import { apiClient } from "@/lib/api/client";
+// import { env } from "@/lib/config/env";
+// import { emptyResponse } from "@/lib/utils/emptyResponse";
+
+// interface Params {
+//   page?: number;
+// }
+
+// export async function getIndianTVShows(
+//   params: Params = {},
+// ): Promise<PaginatedResponse<TMDBTV>> {
+//   const { page = 1 } = params;
+
+//   try {
+//     const res = await apiClient.get<PaginatedResponse<TMDBTV>>("/discover/tv", {
+//       params: {
+//         api_key: env.TMDB_API_KEY,
+//         page,
+//         with_origin_country: "IN",
+//         sort_by: "popularity.desc",
+//       },
+//     });
+
+//     return res.data;
+//   } catch (error) {
+//     console.warn("Indian TV fallback");
+//     return emptyResponse();
+//   }
+// }
+
+//// Fetch Optz
+
 // src/features/movies/services/getIndianTVShows.ts
 
 import { PaginatedResponse } from "@/@types/movie.types";
 import { TMDBTV } from "@/@types/tv.types";
 import { apiClient } from "@/lib/api/client";
-import { env } from "@/lib/config/env";
 import { emptyResponse } from "@/lib/utils/emptyResponse";
 
 interface Params {
@@ -18,16 +53,16 @@ export async function getIndianTVShows(
   try {
     const res = await apiClient.get<PaginatedResponse<TMDBTV>>("/discover/tv", {
       params: {
-        api_key: env.TMDB_API_KEY,
         page,
         with_origin_country: "IN",
         sort_by: "popularity.desc",
       },
+      revalidate: 3600,
     });
 
     return res.data;
   } catch (error) {
-    console.warn("Indian TV fallback");
+    console.warn("Indian TV fallback", error);
     return emptyResponse();
   }
 }

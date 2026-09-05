c36b0c0c037b91a81dcf32367320ecad06596804
Md Faizan Ahmad <mdfaizan7563@gmail.com>
2026-09-05 20:37:04 +0530

    perf: cache Hollywood TV shows for one hour

--- src/features/movies/api/getHollywoodTVShows.ts
+++ src/features/movies/api/getHollywoodTVShows.ts
@@ -1,8 +1,46 @@
+// // src/features/movies/api/getHollywoodTVShows.ts
+
+// import { TMDBTV, PaginatedResponse } from "@/@types/tv.types";
+// import { apiClient } from "@/lib/api/client";
+// import { env } from "@/lib/config/env";
+
+// interface Params {
+//   page?: number;
+// }
+
+// export async function getHollywoodTVShows(
+//   params: Params = {},
+// ): Promise<PaginatedResponse<TMDBTV>> {
+//   const { page = 1 } = params;
+
+//   try {
+//     const res = await apiClient.get<PaginatedResponse<TMDBTV>>("/discover/tv", {
+//       params: {
+//         api_key: env.TMDB_API_KEY,
+//         page,
+//         with_origin_country: "US",
+//         sort_by: "popularity.desc",
+//       },
+//     });
+
+//     return res.data;
+//   } catch (error) {
+//     console.warn("Hollywood TV fallback");
+
+//     return {
+//       page: 1,
+//       results: [],
+//       total_pages: 1,
+//       total_results: 0,
+//     };
+//   }
+// }
+
+/////// Fetch Opt
 // src/features/movies/api/getHollywoodTVShows.ts
 
 import { TMDBTV, PaginatedResponse } from "@/@types/tv.types";
 import { apiClient } from "@/lib/api/client";
-import { env } from "@/lib/config/env";
 
 interface Params {
   page?: number;
@@ -16,16 +54,16 @@ export async function getHollywoodTVShows(
   try {
     const res = await apiClient.get<PaginatedResponse<TMDBTV>>("/discover/tv", {
       params: {
-        api_key: env.TMDB_API_KEY,
         page,
         with_origin_country: "US",
         sort_by: "popularity.desc",
       },
+      revalidate: 3600,
     });
 
     return res.data;
   } catch (error) {
-    console.warn("Hollywood TV fallback");
+    console.warn("Hollywood TV fallback", error);
 
     return {
       page: 1,
